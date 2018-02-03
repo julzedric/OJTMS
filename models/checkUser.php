d54df93d160e25429402a07ed9e43261c9f0cee6
Mona Lisa <walidjamarin21@gmail.com>
2018-02-03 23:10:34 +0800

    check user not working

---
+++ models/checkUser.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Jamarin-PC
+ * Date: 3 Feb 2018
+ * Time: 10:15 PM
+ */
+
+require_once('../connection.php');
+
+$query = mysqli_query($conn, "
+           SELECT  `username`,`email`,`student_id`
+           FROM ojt_users         
+    ");
+$results = array();
+while ($row = mysqli_fetch_assoc($query)){
+    $results[] = $row;
+}
+echo json_encode($results);
