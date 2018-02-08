edebb1abd91e5af6d1234b044dd5bcbdc0c78cdb
Julius <juliuscedricjomena@gmail.com>
2018-02-08 23:20:10 +0800

    initial mailer

---
+++ mailer.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Julius
+ * Date: 08/02/2018
+ * Time: 11:15 PM
+ */
+require 'PHPMailer/PHPMailerAutoload.php';
+
+$mail = new PHPMailer;
+
+$mail->isSMTP();                                   // Set mailer to use SMTP
+$mail->Host = 'smtp.gmail.com';                    // Specify main and backup SMTP servers
+$mail->SMTPAuth = true;                            // Enable SMTP authentication
+$mail->Username = 'crenshawdevelopmentteam@gmail.com';          // SMTP username
+$mail->Password = 'blcakwuilpmmhfnc'; // SMTP password
+$mail->SMTPSecure = 'tls';                         // Enable TLS encryption, `ssl` also accepted
+$mail->Port = 587;                                 // TCP port to connect to
+
+$mail->setFrom('crenshawdevelopmentteam@gmail.com', 'Crenshaw Development Team');
+$mail->addReplyTo('crenshawdevelopmentteam@gmail.com', 'Crenshaw Development Team');
+$mail->addAddress('juliuscedricjomena@gmail.com');   // Add a recipient
+//$mail->addCC('cc@example.com');
+//$mail->addBCC('bcc@example.com');
+
+$mail->isHTML(true);  // Set email format to HTML
+
+$bodyContent = '<h1>How to Send Email using PHP in Localhost by CodexWorld</h1>';
+$bodyContent .= '<p>This is the HTML email sent from localhost using PHP script by <b>CodexWorld</b></p>';
+
+$mail->Subject = 'Email from Localhost by CodexWorld';
+$mail->Body    = $bodyContent;
+
+if(!$mail->send()) {
+    echo 'Message could not be sent.';
+    echo 'Mailer Error: ' . $mail->ErrorInfo;
+} else {
+    echo 'Message has been sent';
+}
