ebe45ff3ad1cc6878f87b18d462b53ed3966fd86
Jhulieann <castillo.jhulieann@gmail.com>
2018-02-11 02:40:38 +0800

    Student Uploading of Step Requirements

---
+++ student/models/submit_requirements.php
@@ -0,0 +1,74 @@
+<?php
+	require_once('../../connection.php');
+	$sql = "SELECT * FROM ojt_requirements_list WHERE `type` = 1 AND `step` = 2";
+        $result = $conn->query($sql);
+
+            if($result->num_rows > 0) 
+            {
+                while($row = $result->fetch_assoc())
+                {
+                  if($_POST) 
+                  {
+
+					$name = $_FILES['name'.$row['id']]['name'];
+					$created_at = date('Y-m-d');
+					$status = 0;
+
+					$link = "../../assets/uploads/student_requirements/";
+					$filename = str_replace(' ', '_', $name);
+					$requirement_id = $_POST['requirement_id'];
+
+					$imageFileType = pathinfo($filename,PATHINFO_EXTENSION);
+
+					// Allow certain file formats
+					if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg" && $imageFileType != "pdf" && $imageFileType != "docx" ) 
+					{
+					    echo "<script type='text/javascript'> 
+								var conf= confirm(\"Sorry, only JPG, JPEG, PNG & PDF files are allowed.\");
+								if(conf == true){
+									window.location.href = 'http://localhost/ojtms/student/index.php';
+								}
+							</script>";
+					    $uploadOk = 0;
+					}else 
+					{
+						$uploadOk = 1;
+					}
+
+					if($uploadOk == 1) 
+					{
+						if(move_uploaded_file($_FILES['name'.$row['id']]['tmp_name'], $link.$filename))
+						 {
+							$sql = "INSERT INTO ojt_student_requirements (name,created_at,link,status,requirement_id)
+							values ('".$name."','".$created_at."','".$link."','".$status."','".$requirement_id."')";
+
+							if($conn->query($sql) == TRUE) 
+							{
+								echo "<script type='text/javascript'> 
+										var conf= confirm(\"Successfully Created.\");
+										if(conf == true){
+											window.location.href = 'http://localhost/ojtms/student/index.php';
+										}
+									</script>";
+							}else 
+							{
+								echo "<script type='text/javascript'> 
+										var conf= confirm(\"Error. Please try again.\");
+										if(conf == true){
+											window.location.href = 'http://localhost/ojtms/student/index.php';
+										}
+									</script>";
+							}
+						}else 
+						{
+							echo "Error";
+						}
+					}
+
+		
+				  }
+				}
+            }
+    $conn->close();
+	
+?>
