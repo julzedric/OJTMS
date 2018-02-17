ad712dd0fe57cf5a6d8991e64ca55e6e45735fd8
julzedric <julzedric@gmail.com>
2018-02-17 00:21:38 -0800

    fix profile pic display

--- includes/students_subheader.php
+++ includes/students_subheader.php
@@ -26,13 +26,16 @@
                 <!-- User Account: style can be found in dropdown.less -->
                 <li class="dropdown user user-menu">
                     <a href="#" class="dropdown-toggle" data-toggle="dropdown">
-                        <img src="../dist/img/user2-160x160.jpg" class="user-image" alt="User Image">
+                        <?php $profPicsmall = $student['profile_picture'] != '' ? "../assets/uploads/profile_picture/".$student['profile_picture']: '../dist/img/user2-160x160.jpg'?>
+
+                        <img src="<?php echo $profPicsmall ;?>" class="user-image" alt="User Image">
                         <span class="hidden-xs"><?php echo ucfirst($_SESSION['username']); ?></span>
                     </a>
                     <ul class="dropdown-menu">
                         <!-- User image -->
                         <li class="user-header">
-                            <img src="../dist/img/user2-160x160.jpg" class="img-circle" alt="User Image">
+                            <?php $profPic = $student['profile_picture'] != '' ? "../assets/uploads/profile_picture/".$student['profile_picture']: '../dist/img/user2-160x160.jpg'?>
+                            <img src="<?php echo $profPic; ?>" class="img-circle" alt="User Image">
 
                             <p>
 
