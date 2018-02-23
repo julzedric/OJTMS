f89963c684e3bf89319fff9933f8ec87285f8906
Julius <juliuscedricjomena@gmail.com>
2018-02-23 11:56:03 +0800

    fix redirect

--- verify.php
+++ verify.php
@@ -29,7 +29,7 @@ if (isset($results)){
             echo "<script type='text/javascript'> 
                     var conf= confirm(\"Success ! You may now Login to your Account..\");
                     if(conf == true){
-                        window.location.href = 'http://ojtms.x10host.com/ojtms/index.php';
+                        window.location.href = 'http://ojtms.x10host.com/index.php';
                     }
                     </script>";
             exit;
